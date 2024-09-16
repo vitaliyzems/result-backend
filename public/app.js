5d3a59563a35a8b79d1ad1e4888deb852e3dd6f9
Vitaliy <vitalyazems@gmail.com>
2024-09-16 19:25:48 +0500

    week-1-additional-task-2

--- public/app.js
+++ public/app.js
@@ -8,18 +8,34 @@ document.addEventListener('click', (event) => {
   }
 
   if (event.target.dataset.type === 'edit') {
+    const note = event.target.closest('li');
     const id = event.target.dataset.id;
-    const editedTitleNode = event.target.closest('li').childNodes[0];
-    const editedTitle = editedTitleNode.textContent.trim();
-    const newTitle = prompt('Enter a new title:', editedTitle);
+    const title = note.childNodes[0].textContent.trim();
+    const initialHtml = note.innerHTML;
 
-    if (!newTitle) {
-      return;
-    }
+    note.innerHTML = `
+      <input type="text" value="${title}" name="title" required></input>
+      <div>
+        <button class="btn btn-success" data-type="save" data-id="<%= notes[i].id %>">Save</button>
+        <button class="btn btn-danger" data-type="cancel" data-id="<%= notes[i].id %>">Cancel</button>
+      </div>`;
 
-    edit(id, newTitle).then(() => {
-      editedTitleNode.textContent = newTitle;
-    });
+    const noteHandle = (event) => {
+      if (event.target.dataset.type === 'cancel') {
+        note.innerHTML = initialHtml;
+        note.removeEventListener('click', noteHandle);
+      } else if (event.target.dataset.type === 'save') {
+        const title = note.querySelector('input').value;
+
+        edit(id, title).then(() => {
+          note.innerHTML = initialHtml;
+          note.removeEventListener('click', noteHandle);
+          note.childNodes[0].textContent = title;
+        });
+      }
+    };
+
+    note.addEventListener('click', noteHandle);
   }
 });
 
